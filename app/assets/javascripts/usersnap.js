c2051e8a368868a70a7ec2e11b93659fd7aaf936
r11runner <r11runner@gmail.com>
2015-08-12 17:58:28 +0200

    moved script from vendor to app/assets/javascripts

--- app/assets/javascripts/usersnap.js
+++ app/assets/javascripts/usersnap.js
@@ -1,4 +1,13 @@
-//= require usersnap
+function loadUsersnap(){
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = '//api.usersnap.com/load/'+
+	    '<%= Rails.application.config.usersnap_api_key %>'+
+            '.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+};
 
 $(document).on('page:change', function(event) {
     var usersnapTags = $(document).find(".include-usersnap");
