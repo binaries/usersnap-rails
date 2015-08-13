570f25f975fd059e0ef62faa5ee08f2f82579c55
r11runner <r11runner@gmail.com>
2015-08-13 17:14:51 +0200

    usersnap.js takes apiKey as parameter; rails_usersnap.js.erb reads the apiKey from the config

--- vendor/assets/javascripts/usersnap.js
+++ vendor/assets/javascripts/usersnap.js
@@ -1,10 +1,8 @@
-function loadUsersnapWidget(){
+function loadUsersnapWidget(apiKey){
     var s = document.createElement("script");
     s.type = "text/javascript";
     s.async = true;
-    s.src = '//api.usersnap.com/load/'+
-	    '<%= Rails.application.config.usersnap_api_key %>'+
-            '.js';
+    s.src = '//api.usersnap.com/load/' + apiKey + '.js';
     var x = document.getElementsByTagName('script')[0];
     x.parentNode.insertBefore(s, x);
 };
